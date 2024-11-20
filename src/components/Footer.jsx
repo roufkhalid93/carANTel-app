ddb262fddfd821d595f216cddae9c69418ba0744
roufkhalid93 <roufkhalid93@gmail.com>
2024-11-20 09:03:25 +0800

    edit responsive design for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,21 @@ export default function Footer() {
     return (
         <div className="footerContainer">
             <div className="socialIcons">
-                <a href="" style={{ backgroundColor: '#ECFFDC' }}><i className="bi bi-facebook"></i></a>
-                <a href="" style={{ backgroundColor: '#ECFFDC' }}><i className="bi bi-instagram"></i></a>
-                <a href="" style={{ backgroundColor: '#ECFFDC' }}><i className="bi bi-twitter"></i></a>
-                <a href="" style={{ backgroundColor: '#ECFFDC' }}><i className="bi bi-youtube"></i></a>
-                <a href="" style={{ backgroundColor: '#ECFFDC' }}><i className="bi bi-tiktok"></i></a>
+                <a href=""><i className="bi bi-facebook"></i></a>
+                <a href=""><i className="bi bi-instagram"></i></a>
+                <a href=""><i className="bi bi-twitter"></i></a>
+                <a href=""><i className="bi bi-youtube"></i></a>
+                <a href=""><i className="bi bi-tiktok"></i></a>
             </div>
             <div className="footerInfo">
                 <ul>
-                    <li><a href="/" style={{ color: '#FFFFF0' }}><strong>Home</strong></a></li>
-                    <li><a href="/promotion" style={{ color: '#FFFFF0' }}><strong>News</strong></a></li>
-                    <li><a href="/aboutus" style={{ color: '#FFFFF0' }}><strong>About Us</strong></a></li>
+                    <li><a href="/"><strong>Home</strong></a></li>
+                    <li><a href="/promotion"><strong>News</strong></a></li>
+                    <li><a href="/aboutus"><strong>About Us</strong></a></li>
                 </ul>
             </div>
             <div className="footerBottom">
-                <p style={{ color: '#FFDEAD' }}>Copyright &copy;2024; Designed by <span className="designer">Abdur Rouf</span></p>
+                <p>Copyright &copy;2024; Designed by <span className="designer">Abdur Rouf</span></p>
             </div>
         </div>
     )
